c9e0c4faafd40f4a7a8ec673e607a82fcdc7b36b
John Dillick <jdillick@resourceammirati.com>
2017-01-23 17:58:01 -0500

    api documentation update.

--- docroot/modules/custom/otc_api/src/ApiController.php
+++ docroot/modules/custom/otc_api/src/ApiController.php
@@ -42,6 +42,57 @@ class ApiController extends ControllerBase {
    * @apiParamExample {url} Request Example:
    *  /api/fun365/category?page=1&recurse=1&depth=2
    *
+   * @apiSuccessExample {json} Paged Categories
+   * HTTP/1.1 200 OK
+   *  {
+   *    "limit": 10,
+   *    "page": 0,
+   *    "count": 2,
+   *    "results": [
+   *      {
+   *        "parent": "",
+   *        "uuid": "da593707-2796-4f03-b75f-59a1515917b4",
+   *        "name": "Parent Category",
+   *        "description": null,
+   *        "weight": 0,
+   *        "changed": "2016-12-07T13:09:00-0600",
+   *        "field_3200x1391_img": {
+   *          "full": "http://example.com/path/to/image",
+   *          ...
+   *        },
+   *        "field_828x828_img": null,
+   *        "field_contributor": {
+   *          "uuid": "abc123456-1234-a1b2-a2b3c4e517c4",
+   *          "type": "contributor"
+   *        },
+   *        "field_cta_link": {
+   *          "url": "http://example.com",
+   *          "title": "Example Title"
+   *        },
+   *        "field_description": "&lt;p&gt;Category Description&lt;/p&gt;",
+   *        "field_display_title": "Parent Category",
+   *        "field_enable_fan_reel": false,
+   *        "field_featured_content": [
+   *          {
+   *            "uuid": "fe593707-1234-4e03-c7da-59a1515917c4",
+   *            "type": "featured_content"
+   *          }
+   *        ],
+   *        ...
+   *      },
+   *      {
+   *        "parent": "da593707-2796-4f03-b75f-59a1515917b4",
+   *        "uuid": "f31b8dd9-9aae-42c5-903d-410064005b12",
+   *        "name": "Child Category",
+   *        "description": null,
+   *        "weight": 0,
+   *        "changed": "2016-12-07T13:09:10-0600",
+   *        "field_3200x1391_img": null,
+   *        ...
+   *      }
+   *    ]
+   *  }
+   *
    * @param  Request $request the request
    * @return CacheableJsonResponse
    */
@@ -73,6 +124,24 @@ class ApiController extends ControllerBase {
    * @apiParamExample {url} Request Example:
    *  /api/fun365/category/da593707-2796-4f03-b75f-59a1515917b4/content?page=1&recurse=1&depth=2
    *
+   * @apiSuccessExample {json} Paged Category Content
+   * HTTP/1.1 200 OK
+   *  {
+   *    "limit": 10,
+   *    "page": 0,
+   *    "published": true,
+   *    "count": 2,
+   *    "results": [
+   *      {
+   *        "uuid": "ccf0cb1a-64e4-456c-8048-971dd0d86ee8",
+   *        "type": "project"
+   *      },
+   *      {
+   *      ...
+   *      }
+   *    ]
+   *  }
+   *
    * @param  Request $request the request
    * @return CacheableJsonResponse
    */
@@ -94,7 +163,7 @@ class ApiController extends ControllerBase {
   /**
    * @apiDescription Base tag api call.
    *
-   * @api {get} /api/fun365/tag Request paginated list of categories.
+   * @api {get} /api/fun365/tag Request paginated list of tags.
    * @apiName All
    * @apiGroup Tag
    * @apiParam {Number} page GET param  the result page (default 0)
@@ -102,6 +171,27 @@ class ApiController extends ControllerBase {
    * @apiParam {Number} depth GET param levels deep to limit recursion (default 2)
    * @apiParamExample {url} Request Example:
    *  /api/fun365/tag?page=1&recurse=1&depth=2
+   *
+   * @apiSuccessExample {json} Paged Tags
+   * HTTP/1.1 200 OK
+   *  {
+   *    "limit": 10,
+   *    "page": 0,
+   *    "count": 2,
+   *    "results": [
+   *      {
+   *        "parent": "",
+   *        "uuid": "ae9dc78c-074a-48d8-a6bd-e7a7924f155f",
+   *        "name": "Christmas",
+   *        "description": null,
+   *        "weight": 0,
+   *        "changed": "2016-12-12T13:31:05-0600"
+   *      },
+   *      {
+   *        ...
+   *      }
+   *    ]
+   *  }
    *
    * @param  Request $request the request
    * @return CacheableJsonResponse
@@ -131,6 +221,17 @@ class ApiController extends ControllerBase {
    * @apiParam {String} uuid Universally Unique ID for tag
    * @apiParamExample {url} Request Example:
    *  /api/fun365/tag/ae9dc78c-074a-48d8-a6bd-e7a7924f155f
+   *
+   * @apiSuccessExample {json} A Tag
+   * HTTP/1.1 200 OK
+   *  {
+   *   "parent": "",
+   *   "uuid": "ae9dc78c-074a-48d8-a6bd-e7a7924f155f",
+   *   "name": "Christmas",
+   *   "description": null,
+   *   "weight": 0,
+   *   "changed": "2016-12-12T13:31:05-0600"
+   *  }
    *
    * @param  Request $request the request
    * @return CacheableJsonResponse
@@ -162,6 +263,24 @@ class ApiController extends ControllerBase {
    * @apiParamExample {url} Request Example:
    *  /api/fun365/tag/ae9dc78c-074a-48d8-a6bd-e7a7924f155f/content?page=1&recurse=1&depth=2
    *
+   * @apiSuccessExample {json} Tagged Content
+   * HTTP/1.1 200 OK
+   *  {
+   *    "limit": 10,
+   *    "page": 0,
+   *    "published": true,
+   *    "count": 2,
+   *    "results": [
+   *      {
+   *        "uuid": "ccf0cb1a-64e4-456c-8048-971dd0d86ee8",
+   *        "type": "project"
+   *      },
+   *      {
+   *      ...
+   *      }
+   *    ]
+   *  }
+   *
    * @param  Request $request the request
    * @return CacheableJsonResponse
    */
@@ -190,6 +309,35 @@ class ApiController extends ControllerBase {
    * @apiParam {String} uuid Universally Unique ID for tag
    * @apiParamExample {url} Request Example:
    *  /api/fun365/tag/da593707-2796-4f03-b75f-59a1515917b4
+   *
+   * @apiSuccessExample [json] Paged Categories
+   *  HTTP/1.1 200 OK
+   *  {
+   *    "limit": 10,
+   *    "page": 0,
+   *    "count": 2,
+   *    "results": [
+   *      {
+   *        "parent": "da593707-2796-4f03-b75f-59a1515917b4",
+   *        "uuid": "f31b8dd9-9aae-42c5-903d-410064005b12",
+   *        "name": "Child Category",
+   *        "description": null,
+   *        "weight": 0,
+   *        "changed": "2016-12-07T13:09:10-0600",
+   *        "field_3200x1391_img": {
+   *          "full": "http://example.com/path/to/image",
+   *          ...
+   *        },
+   *        ...
+   *      },
+   *      {
+   *        "parent": "",
+   *        "uuid": "da593707-2796-4f03-b75f-59a1515917b4",
+   *        "name": "Parent Category",
+   *        ...
+   *      },
+   *    ]
+   *  }
    *
    * @param  Request $request the request
    * @return CacheableJsonResponse
@@ -324,6 +472,73 @@ class ApiController extends ControllerBase {
    *
    * // Specified project, no recursion:
    *  /api/fun365/project/ccf0cb1a-64e4-456c-8048-971dd0d86ee8
+   *
+   * @apiSuccessExample {json} Specified Content
+   * HTTP/1.1 200 OK
+   *  {
+   *    "uuid": "ccf0cb1a-64e4-456c-8048-971dd0d86ee8",
+   *    "type": "project",
+   *    "status": true,
+   *    "created": "2017-01-23T13:26:37-0600",
+   *    "changed": "2017-01-23T13:26:37-0600",
+   *    "field_828x828_img": {
+   *      "full": "http://otc.prod.acquia-sites.com/sites/default/files/projects/hero/mobile/2x/828x828_0.png",
+   *      "400x400_img": "http://otc.prod.acquia-sites.com/sites/default/files/styles/400x400_img/public/projects/hero/mobile/2x/828x828_0.png?itok=-C5ACjBN",
+   *      "414x414_img": "http://otc.prod.acquia-sites.com/sites/default/files/styles/414x414_img/public/projects/hero/mobile/2x/828x828_0.png?itok=gdjmy1SZ",
+   *      "448x448_img": "http://otc.prod.acquia-sites.com/sites/default/files/styles/448x448_img/public/projects/hero/mobile/2x/828x828_0.png?itok=OayjPFvG"
+   *    },
+   *    "field_896x896_img": {
+   *      "full": "http://otc.prod.acquia-sites.com/sites/default/files/projects/tile/desktop/2x/896x896_0.png",
+   *      ...
+   *    },
+   *    "field_900x677_img": null,
+   *    "field_category": [
+   *      {
+   *        "uuid": "da593707-2796-4f03-b75f-59a1515917b4",
+   *        "type": "category"
+   *      },
+   *      ...
+   *    ],
+   *    "field_contributor": {
+   *      "uuid": "abc93123-1234-4f03-a15f-abc1515917b4",
+   *      "type": "contributor"
+   *    },
+   *    "field_description": "&lt;p&gt;Test Project description&lt;/p&gt;\r\n",
+   *    "field_display_title": "Test Project",
+   *    "field_download_file": "http://otc.prod.acquia-sites.com/sites/default/files/downloads/pdf/OTC%20Content%20Management%20Integration%20Overview%20%28Updated%2012-2-2016%29.pdf",
+   *    "field_items_needed": [
+   *      "Item 1",
+   *      "Item 2"
+   *    ],
+   *    "field_meta_description": "&lt;p&gt;Test Project description&lt;/p&gt;\r\n",
+   *    "field_meta_title": "Test Project",
+   *    "field_products": [
+   *      {
+   *        "uuid": "a55ca0f4-ede6-469a-b763-093f6b36d7ac",
+   *        "type": "product"
+   *      },
+   *      ...
+   *    ],
+   *    "field_project_lite": false,
+   *    "field_skill": "easy",
+   *    "field_skyword_related_article": {
+   *    "url": "http://example.com",
+   *    "title": "Skyword Related Article \"Read More\" Link Text"
+   *    },
+   *    "field_skyword_related_look": {
+   *    "url": "http://example.com",
+   *    "title": "Skyword Related Look Link Text"
+   *    },
+   *    "field_step": [
+   *      {
+   *        "uuid": "5197a8e1-74ca-4071-8e8f-8068580c1194",
+   *        "type": "step"
+   *      },
+   *      ...
+   *    ],
+   *    "field_suppress_fanreel": false,
+   *    ...
+   *  }
    *
    * @param  Request $request     the request
    * @param  string  $contentType the content type
