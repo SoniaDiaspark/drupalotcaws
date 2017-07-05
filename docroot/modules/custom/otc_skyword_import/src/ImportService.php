df4f17e3d50c49bf4451e23f3de626dd2d28a65a
Jesse Hemminger <jhemminger@resourceammirati.com>
2017-07-05 18:11:10 -0400

    OTC-736 update skyward feed importer WIP recipies

--- docroot/modules/custom/otc_skyword_import/src/ImportService.php
+++ docroot/modules/custom/otc_skyword_import/src/ImportService.php
@@ -107,7 +107,7 @@ class ImportService {
 
       foreach ($this->mapImports($simplexml) as $type => $docs) {
         foreach ($docs as $doc) {
-          print_r($doc);
+          // print_r($doc);
           // $this->queueImportJob($type, $doc);
           // $this->create($doc, $type);
         }
@@ -158,10 +158,10 @@ class ImportService {
           break;
         case 'Project':
         case 'Project-Lite':
-          $docs['project'][] = $this->mappingService->get('project')->map($document);
+          // $docs['project'][] = $this->mappingService->get('project')->map($document);
           break;
         case 'fun365recipe':
-          // $docs['recipe'][] = $this->mappingService->get('recipe')->map($document);
+          $docs['recipe'][] = $this->mappingService->get('recipe')->map($document);
           break;
         default:
       }
