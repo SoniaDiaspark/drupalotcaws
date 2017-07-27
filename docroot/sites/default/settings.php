79dc5ad4cf3053c8f9b73201c962a75def5c43da
sandeep kumar <sandeep.kumar@webdunia.net>
2017-07-27 14:48:17 +0530

    Error reporting on

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -1,5 +1,8 @@
 <?php
-
+error_reporting(-1);  // Have PHP complain about absolutely everything 
+$conf['error_level'] = 2;  // Show all messages on your screen, 2 = ERROR_REPORTING_DISPLAY_ALL.
+ini_set('display_errors', TRUE);  // These lines just give you content on WSOD pages.
+ini_set('display_startup_errors', TRUE);
 /**
  * @file
  * Drupal site-specific configuration file.
@@ -786,3 +789,5 @@ if (file_exists('/var/www/site-php')) {
   // Memcache settings.
   $settings['cache']['default'] = 'cache.backend.memcache';
 }
+
+$config['system.logging']['error_level'] = 'verbose';
