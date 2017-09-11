15ec076c3905d50a40e1a812401f8db03236f087
sandeep kumar <sandeep.kumar@webdunia.net>
2017-09-11 15:53:16 +0530

    Enable debugging

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -1,5 +1,8 @@
 <?php
-
+error_reporting(-1);  // Have PHP complain about absolutely everything 
+$conf['error_level'] = 2;  // Show all messages on your screen, 2 = ERROR_REPORTING_DISPLAY_ALL.
+ini_set('display_errors', TRUE);  // These lines just give you content on WSOD pages.
+ini_set('display_startup_errors', TRUE);
 /**
  * @file
  * Drupal site-specific configuration file.
@@ -788,3 +791,5 @@ if (file_exists('/var/www/site-php')) {
     $settings['cache']['default'] = 'cache.backend.memcache';
   }  
 }
+$conf['error_level'] = 2; 
+$config['system.logging']['error_level'] = 'verbose';
