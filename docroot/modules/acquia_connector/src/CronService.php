2004cf4e60317d77752919b80bab9eb07ede3c53
arvind.kinja <arvind.kinja@diaspark.com>
2019-02-10 00:26:05 +0530

    Patch applied on acquia connector module provided by support team.

--- docroot/modules/acquia_connector/src/CronService.php
+++ docroot/modules/acquia_connector/src/CronService.php
@@ -20,8 +20,15 @@ class CronService implements LoggerInterface {
     // Make sure that even when cron failures prevent hook_cron() from being
     // called, we still send out a heartbeat.
     if (!empty($context['channel']) && ($context['channel'] == 'cron') && ($message == 'Attempting to re-run cron while it is already running.')) {
-      $subscription = new Subscription();
-      $subscription->update();
+      // Added by Arvind Kinja on 16-jan-2019.
+      // $subscription = new Subscription();
+      // $subscription->update();
+      // Avoid doing this too frequently.
+      $last_update_attempt = \Drupal::state()->get('acquia_subscription_data.timestamp', false);
+      if (!$last_update_attempt || ((REQUEST_TIME - $last_update_attempt) >= 60 * 60)) {
+        $subscription = new Subscription();
+        $subscription->update();
+      }
     }
   }
 
